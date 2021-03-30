944ab20d8cafdf87955c0362e166dc87a2295963
Ali Parlakci <parlakciali@gmail.com>
2021-03-30 18:50:19 +0300

    hw4: initial commit

---
+++ homework4/aliparlakci_Parlakci_Ali_hw4.cpp
@@ -0,0 +1,20 @@
+/*
+ * CS204 Spring 2020 - Albert Levi
+ * Homework 4
+ * 30 March 2021
+ * Written by Ali PARLAKCI
+ * 28114, aliparlakci@sabanciuniv.edu
+ */
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "HW4" << endl;
+
+	return 0;
+}
---
+++ homework4/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+./build.sh
+
+if [ $? -eq 0 ]
+then
+    for i in {1..7}
+    do  
+        cat "./tests/input$i.txt" | ./aliparlakci_Parlakci_Ali_hw4 | diff -Z --strip-trailing-cr "./tests/output$i.txt" -
+
+        if [ $? -eq 0 ]
+        then
+            echo "SUCCESSFUL: test $i"
+        else
+            echo "FAILED: test $i"
+        fi
+        
+    done
+fi
